163dcd3d0ad67f385fb992dd0913cd2cde76197a
akram moncer <akram.moncer@tritux.com>
2014-12-08 17:54:27 +0100

    add pull commande to the install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ fin () {
 }
 [ "$#" -eq 1 ] || fin "il faut indiquer un seul chemain d'installation du shared-lib"
 [ -d "$1" ] || fin "le dossier $1 n'existe pas"
+git pull origin master
 mvn clean install -Dtomcat.sharedlib.folder=$1
 array=($(ls ./target/rpm/ot-sharedlib/RPMS/noarch))
 rpm -Uvh /tmp/ot-sharedlib/target/rpm/ot-sharedlib/RPMS/noarch/$array
